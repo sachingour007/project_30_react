0b4f5b9339405019382bce442eb4d18ae44fd876
sachingour <gour.sachin.sg@gmail.com>
2024-01-20 22:20:10 +0530

    timer App

--- src/components/DigitalClock/TimerApp.jsx
+++ src/components/DigitalClock/TimerApp.jsx
@@ -3,10 +3,41 @@ import styles from "./clock.module.css";
 
 const TimerApp = () => {
   const [time, setTime] = useState(0);
+  const [flag, setFlag] = useState(false);
 
   useEffect(() => {
-   
-  }, []);
+    let timer;
+    if (flag) {
+      timer = setInterval(() => {
+        setTime(time + 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  });
+
+  const startHanlder = () => {
+    setFlag(true);
+  };
+  const stopHandler = () => {
+    setTime(0);
+    setFlag(false);
+  };
+  const pauseHandler = () => {
+    setFlag(false);
+  };
+
+  //Helper Function
+
+  const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+      2,
+      "0"
+    )}:${String(remainingSeconds).padStart(2, "0")}`;
+  };
 
   return (
     <div className={styles.main_Container}>
@@ -16,11 +47,17 @@ const TimerApp = () => {
             <h1>Digital-Clock</h1>
           </div>
           <div className={styles.watch}>
-            <h2>{time}</h2>
+            <h2> {formatTime(time)}</h2>
             <div className={styles.buttons}>
-              <button className={styles.start}>Start</button>
-              <button className={styles.stop}>Stop</button>
-              <button className={styles.pause}>pause</button>
+              <button className={styles.start} onClick={startHanlder}>
+                Start
+              </button>
+              <button className={styles.stop} onClick={stopHandler}>
+                Stop
+              </button>
+              <button className={styles.pause} onClick={pauseHandler}>
+                pause
+              </button>
             </div>
           </div>
         </div>
